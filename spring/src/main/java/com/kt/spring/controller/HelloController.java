48da9bc4d2e383a76086dccccfbf4d42ba1a5c21
minheechung <minhee7271@naver.com>
2025-02-19 19:28:11 +0900

    feat: create api

--- spring/src/main/java/com/kt/spring/controller/HelloController.java
+++ spring/src/main/java/com/kt/spring/controller/HelloController.java
@@ -2,15 +2,21 @@ package com.kt.spring.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
+@RequestMapping("/mini")
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String HelloMini() {
+        System.out.println("gg>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         return "Hello, Mini!";
-    }
-    
+    } 
+
+
+
 
 
 }
